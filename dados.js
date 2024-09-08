fbab0c894b846abf8435ddce96509e628b4be232
JhenMachado <jhenifermachado_@hotmail.com>
2024-09-07 22:31:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+
+        {
+          titulo: "Avenged Sevenfold",
+          descricao: "O Avenged Sevenfold, ou A7X, é uma das bandas de heavy metal mais influentes e populares da atualidade.",
+          link: "https://pt.wikipedia.org/wiki/Avenged_Sevenfold",
+          integrantes: "vocalista M. Shadows, nas guitarras Zacky Vengeance e Synyster Gates, no baixo Johnny Christ e na bateria Brooks Wackerman.",
+          tags: "A7x"
+        },
+        {
+          titulo: "Evanescence",
+          descricao: "Evanescence é uma banda de rock alternativo conhecida por sua sonoridade melancólica e a poderosa voz de Amy Lee.",
+          link: "https://pt.wikipedia.org/wiki/Evanescence",
+          integrantes: "vocalista Amy Lee, guitarrista Terry Balsamo, baixista Tim McCord, baterista Will Hunt e tecladista Troy McLawhorn.",
+          tags: "Amy Lee melancolica"
+        },
+        {
+          titulo: "Deep Purple",
+          descricao: "Deep Purple é uma das bandas de rock mais importantes da história, pioneira no hard rock e heavy metal.",
+          link: "https://pt.wikipedia.org/wiki/Deep_Purple",
+          integrantes: "vocalista Ian Gillan, guitarrista Steve Morse, baixista Roger Glover, tecladista Don Airey e baterista Ian Paice.",
+          tags: "hard rock heavy metal"
+        },
+        {
+          titulo: "Journey",
+          descricao: "Journey é uma banda de rock americana conhecida por seus hits radiofônicos e a poderosa voz de Steve Perry.",
+          link: "https://pt.wikipedia.org/wiki.Journey",
+          integrantes: "vocalista Arnel Pineda, guitarrista Neal Schon, baixista Ross Valory, tecladista Jonathan Cain e baterista Steve Smith.",
+          tags: "Steve radiofonicos"
+        },
+        {
+          titulo: "Incubus",
+          descricao: "Incubus é uma banda de alternative metal conhecida por sua sonoridade experimental e letras poéticas.",
+          link: "https://pt.wikipedia.org/wiki/Incubus",
+          integrantes: "vocalista Brandon Boyd, guitarrista Mike Einziger, baixista Jose Pascual, baterista Jose Pasillas II e DJ Chris Kilmore.",
+          tags:"poeticas"
+        }
+      ];
+
